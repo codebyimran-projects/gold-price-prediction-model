a6c8431510c0c18a5162ce585cee8fcd3c9e07c7
Muhammad imran <muhammadimran27584@gmail.com>
2026-02-08 00:27:45 +0500

    update

--- main.py
+++ main.py
@@ -13,18 +13,47 @@ print(gld_price_data.head())
 print(gld_price_data.shape)
 # check null values 
 print(gld_price_data.isnull().sum())
-
+gld_price_data = gld_price_data.drop('Date', axis=1)
 correlation = gld_price_data.corr()
 
-plt.figure(figsize=(8, 8))
-sns.heatmap(
-    correlation,
-    annot=True,
-    fmt='.1f',
-    cmap='Blues',
-    square=True,
-    cbar=True,
-    annot_kws={'size': 8}
+# plt.figure(figsize=(8, 8))
+# sns.heatmap(
+#     correlation,
+#     annot=True,
+#     fmt='.1f',
+#     cmap='Blues',
+#     square=True,
+#     cbar=True,
+#     annot_kws={'size': 8}
+# )
+
+# plt.show()
+
+# print(correlation['GLD'])
+
+# splitting data into two var 
+X = gld_price_data.drop('GLD', axis=1)
+y = gld_price_data['GLD']
+
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=2
 )
 
-plt.show()
+print (X_train.shape)
+
+
+
+model = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+pred_data = model.predict(X_test)
+
+r2_error = metrics.r2_score(y_test, pred_data)
+
+print("R square error:", r2_error)
